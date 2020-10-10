630015bb7d55a23e1b4591a3aa3de451d4e108ae
fuggyme <xiepei17@mails.ucas.ac.cn>
2020-10-10 11:30:01 +0800

    Update ftpServer.py

--- bin/ftpServer.py
+++ bin/ftpServer.py
@@ -4,14 +4,26 @@
 
 import sys
 import os
+import socket
 from core import socket_server
 
 path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(path)
 
 
+def get_host_ip():
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.connect(('8.8.8.8', 80))
+        ip = s.getsockname()[0]
+    finally:
+        s.close()
+
+    return ip
+
 
 if __name__ == "__main__":
-    HOST, PORT = "10.1.0.74", 9901
+    print("服务器地址", get_host_ip())
+    HOST, PORT = get_host_ip(), 9901
     server = socket_server.socketserver.ThreadingTCPServer((HOST, PORT), socket_server.MyTCPServer)
     server.serve_forever()
